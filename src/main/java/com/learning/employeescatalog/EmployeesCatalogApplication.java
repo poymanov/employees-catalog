702d46303001ea0b32f1b68f3882fb0a2e76be2b
poymanov <n.poymanov@gmail.com>
2019-09-19 22:39:44 +0300

    Init application

---
+++ src/main/java/com/learning/employeescatalog/EmployeesCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.learning.employeescatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/employeescatalog/EmployeesCatalogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learning.employeescatalog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeesCatalogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
