d7aea519186a9c0e424bf87f7a6426b15b8649d2
poymanov <n.poymanov@gmail.com>
2019-09-22 13:50:12 +0300

    Added employee DAO JPA implementation

--- src/main/java/com/learning/employeescatalog/service/EmployeeServiceImpl.java
+++ src/main/java/com/learning/employeescatalog/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.learning.employeescatalog.service;
 import com.learning.employeescatalog.dao.EmployeeDAO;
 import com.learning.employeescatalog.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +15,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeDAO employeeDAO;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+    public EmployeeServiceImpl(@Qualifier("employeeDAOJpa") EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
 
