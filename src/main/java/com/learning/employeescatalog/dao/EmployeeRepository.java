8ca9a6b606008ddf599ea796c10aeff229069a0f
poymanov <n.poymanov@gmail.com>
2019-09-23 20:19:45 +0300

    Replaced rest controller and service with Spring Data REST

--- src/main/java/com/learning/employeescatalog/dao/EmployeeRepository.java
+++ src/main/java/com/learning/employeescatalog/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.learning.employeescatalog.dao;
 
 import com.learning.employeescatalog.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
